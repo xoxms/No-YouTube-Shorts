56a5e6e952aff27054f03e6b2c445d2b28de11eb
Kuuuuuuuu <notkungz.suphakorn@gmail.com>
2022-08-21 03:19:44 +0700

    Migrate to Chrome Manifest V3 and use Regex instead url

--- src/inject.js
+++ src/inject.js
@@ -1,35 +1,36 @@
-const shorts = "https://www.youtube.com/shorts/";
+const shortsRegex = /https:\/\/www\.youtube\.com\/shorts\//;
 let url;
+
 setInterval(function () {
-	run_2();
-	run_1();
+    run_2();
+    run_1();
 }, 3000);
 
 function run_1() {
-	url = window.location.href;
-	if (isShorts(url)) {
-		window.location.href = convert(url);
-	}
+    url = window.location.href;
+    if (isShorts(url)) {
+        window.location.href = convert(url);
+    }
 }
 
 function run_2() {
-	const elements = document.getElementsByTagName("a");
-	for (let i = 0; i < elements.length; i++) {
-		const element = elements[i];
-		if (isShorts(element.href)) {
-			element.href = convert(element.href);
-		}
-	}
+    const elements = document.getElementsByTagName("a");
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        if (isShorts(element.href)) {
+            element.href = convert(element.href);
+        }
+    }
 }
 
 function convert(link) {
-	return `https://www.youtube.com/watch?v=${getVideoId(link)}`;
+    return `https://www.youtube.com/watch?v=${getVideoId(link)}`;
 }
 
 function getVideoId(link) {
-	return link.split(shorts)[1];
+    return link.split(shortsRegex)[1];
 }
 
 function isShorts(link) {
-	return link.startsWith(shorts);
+    return link.match(shortsRegex);
 }
